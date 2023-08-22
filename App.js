e43ce90fd7d452b11a0fcdc6b9cf020035395b5c
Shreeja Das <88001308+shreejalearn@users.noreply.github.com>
2023-08-22 15:32:25 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,66 +1,123 @@
-import { useState } from 'react';
-import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  ImageBackground,
+  SafeAreaView,
+  View,
+} from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useFonts } from 'expo-font';
 import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
-
+import PreGameScreen from './screens/PreGameScreen';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
+import GameScreenFriend from './screens/GameScreenFriend';
 import GameOverScreen from './screens/GameOverScreen';
+import GameOverScreenFriend from './screens/GameOverScreenFriend';
 import Colors from './constants/colors';
 
 export default function App() {
+  const [currentScreen, setCurrentScreen] = useState('preGame');
+  const [playingWithComputer, setPlayingWithComputer] = useState(true);
   const [userNumber, setUserNumber] = useState();
-  const [gameIsOver, setGameIsOver] = useState(true);
   const [guessRounds, setGuessRounds] = useState(0);
-
   const [fontsLoaded] = useFonts({
     'open-sans': require('./assets/fonts/CabinSketch-Bold.ttf'),
     'open-sans-bold': require('./assets/fonts/CabinSketch-Bold.ttf'),
     'real-open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
   });
 
+  useEffect(() => {
+    if (currentScreen === 'gameOver' && guessRounds > 0) {
+      console.log(guessRounds);
+      console.log(userNumber);
+    }
+  }, [currentScreen, guessRounds]);
+
+
   if (!fontsLoaded) {
     return <AppLoading />;
   }
 
-  function pickedNumberHandler(pickedNumber) {
-    setUserNumber(pickedNumber);
-    setGameIsOver(false);
-  }
+  let screen;
 
-  function gameOverHandler(numberOfRounds) {
-    setGameIsOver(true);
-    setGuessRounds(numberOfRounds);
-  }
+  switch (currentScreen) {
+    case 'preGame':
+      screen = (
+        <PreGameScreen
+          onStartGame={() => setCurrentScreen('startGame')}
+          onPlayWithComputer={() => {
+            setPlayingWithComputer(true);
+            setCurrentScreen('startGame');
+          }}
+          onPlayWithFriend={() => {
+            setPlayingWithComputer(false);
+            setCurrentScreen('startGame');
+          }}
+        />
+      );
+      break;
+    case 'startGame':
+      screen = (
+        <StartGameScreen
+          onPickNumber={(pickedNumber) => {
+            setUserNumber(pickedNumber);
+            setCurrentScreen(playingWithComputer ? 'gameComputer' : 'gameFriend');
+          }}
+        />
+      );
+      break;
+      case 'gameComputer':
+        screen = (
+          <GameScreen
+            userNumber={userNumber}
+            onGameOver={(numberOfRounds) => {
+              setGuessRounds(numberOfRounds);
+              setCurrentScreen('gameOver', numberOfRounds);
+            }}
+          />
 
-  function startNewGameHandler() {
-    setUserNumber(null);
-    setGuessRounds(0);
-  }
+        );        
+        break;
+      case 'gameFriend':
+      screen = (
+        <GameScreenFriend
+          userNumber={userNumber}
+          onGameOver={(numberOfRounds) => {
+            setGuessRounds(numberOfRounds);
+            setCurrentScreen('gameOverFriend', numberOfRounds);
+          }}
+        />
+      );
+      break;
+    case 'gameOver':
 
-  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
+      screen = (
 
-  if (userNumber) {
-    screen = (
-      <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />
-    );
-  }
-
-  if (gameIsOver && userNumber) {
-    screen = (
-      <GameOverScreen
-        userNumber={userNumber}
-        roundsNumber={guessRounds}
-        onStartNewGame={startNewGameHandler}
-      />
-    );
+        <GameOverScreen
+          userNumber={userNumber}
+          roundsNumber={guessRounds}
+          onStartNewGame={() => setCurrentScreen('preGame')}
+        />
+      );
+      break;
+    case 'gameOverFriend':
+      screen = (
+        <GameOverScreenFriend
+          userNumber={userNumber}
+          roundsNumber={guessRounds}
+          onStartNewGame={() => setCurrentScreen('preGame')}
+        />
+      );
+      break;
+    default:
+      screen = <PreGameScreen />;
   }
 
   return (
     <>
-      <StatusBar style="dark"  />
+      <StatusBar style="light" />
       <LinearGradient
         colors={[Colors.primary500, Colors.primary600]}
         style={styles.rootScreen}
